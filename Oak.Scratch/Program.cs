974c53d70b2eb8cd14131eecb2b4b7dcb5dfea36
Daniel Robinson <dan7c2@gmail.com>
2023-02-22 21:32:14 +0000

    remove unused usings

--- Oak.Scratch/Program.cs
+++ Oak.Scratch/Program.cs
@@ -1,10 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Common;
 using Oak.Flow;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
 
 var flow = Flow.FromJson(File.ReadAllText("test.json"));
 var str = flow.ToJson(true);
