d37b15f31fc47b4f4e08ac8b20e4b8c8f6add628
Daniel Robinson <dan7c2@gmail.com>
2023-03-18 11:48:22 +0000

    use shared server run

--- Oak.Server/Program.cs
+++ Oak.Server/Program.cs
@@ -1,25 +1,6 @@
-using Oak.I18n;
-using Oak.Service.Services;
 using Common.Server;
+using Oak.Service.Services;
 using Oak.Db;
+using Oak.I18n;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddApiServices<OakDb>(S.UnexpectedError);
-
-var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseHsts();
-}
-app.UseHttpsRedirection();
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
-app.UseRouting();
-app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
-app.MapGrpcService<ApiService>();
-app.MapFallbackToFile("index.html");
-app.Run(Config.Server.Listen);
+Server.Run<OakDb, ApiService>(args, S.UnexpectedError);
