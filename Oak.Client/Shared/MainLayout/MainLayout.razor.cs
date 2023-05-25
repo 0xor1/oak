21922c05b8e4eb302d819620040ba3460c1feb17
Daniel Robinson <dan7c2@gmail.com>
2023-05-25 20:51:45 +0100

    parse out url params for core org , project and task ids

---
+++ Oak.Client/Shared/MainLayout/MainLayout.razor.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Oak.Client.Shared.MainLayout;
+
+public partial class MainLayout
+{
+    [GeneratedRegex(@"/org/([^/\s]+)")]
+    private static partial Regex OrgId();
+
+    [GeneratedRegex(@"/project/([^/\s]+)")]
+    private static partial Regex ProjectId();
+
+    [GeneratedRegex(@"/task/([^/\s]+)")]
+    private static partial Regex TaskId();
+}
