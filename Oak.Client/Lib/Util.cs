65119014ff058c9b0ffebcdec8d653da43188767
Daniel Robinson <dan7c2@gmail.com>
2023-05-27 17:32:06 +0100

    work on projects

--- Oak.Client/Lib/Util.cs
+++ Oak.Client/Lib/Util.cs
@@ -1,13 +1,60 @@
-﻿namespace Oak.Client.Lib;
+﻿using Common.Shared;
+
+namespace Oak.Client.Lib;
 
 public static class Util
 {
-    public static void Time(ulong val, uint? hoursPerDay, uint? daysPerWeek)
+    public static string Time(ulong val, uint hoursPerDay, uint daysPerWeek)
     {
-        if (hoursPerDay > 0 && daysPerWeek > 0)
+        Throw.OpIf(hoursPerDay == 0 || daysPerWeek == 0, "hoursPerDay and daysPerWeek must be > 0");
+        const ulong minsPerHour = 60ul;
+        const ulong weeksPerYear = 52ul;
+        var minsPerDay = minsPerHour * hoursPerDay;
+        var minsPerWeek = minsPerDay * daysPerWeek;
+        var minsPerYear = minsPerWeek * weeksPerYear;
+
+        // TODO i18n/l10n this content
+        if (val >= minsPerYear)
         {
-            var weeksPerYear = 52ul;
-            // render as #y #w #d #h #m
+            return $"{((decimal)val / minsPerYear).ToString("F1")}y";
         }
+        if (val >= minsPerWeek)
+        {
+            return $"{((decimal)val / minsPerWeek).ToString("F1")}w";
+        }
+        if (val >= minsPerDay)
+        {
+            return $"{((decimal)val / minsPerWeek).ToString("F1")}d";
+        }
+        if (val >= minsPerHour)
+        {
+            return $"{((decimal)val / minsPerHour).ToString("F1")}h";
+        }
+        return $"{val}m";
+    }
+
+    public static string Cost(ulong val, string symbol)
+    {
+        const ulong t = 1000000000000ul;
+        const ulong b = 1000000000ul;
+        const ulong m = 1000000ul;
+        const ulong k = 1000ul;
+        if (val >= t)
+        {
+            return $"{symbol}{((decimal)val / t).ToString("F")}T";
+        }
+        if (val >= b)
+        {
+            return $"{symbol}{((decimal)val / b).ToString("F")}B";
+        }
+        if (val >= m)
+        {
+            return $"{symbol}{((decimal)val / m).ToString("F")}M";
+        }
+        if (val >= k)
+        {
+            return $"{symbol}{((decimal)val / k).ToString("F")}K";
+        }
+        return $"{symbol}{val}";
     }
 }
