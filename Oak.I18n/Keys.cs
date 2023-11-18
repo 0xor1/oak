92d3e9204618bdd262fb160620344e13974fa91d
Daniel Robinson <dan7c2@gmail.com>
2023-11-18 18:29:15 +0000

    use i18n code gen

--- Oak.I18n/Keys.cs
+++ Oak.I18n/Keys.cs
@@ -1,139 +1,124 @@
-﻿namespace Oak.I18n;
+// Generated Code File, Do Not Edit.
+// This file is generated with Common.I18nCodeGen.
+
+using Common.Shared;
+
+namespace Oak.I18n;
 
 public static partial class S
-{
-    public const string HomeHeader = "home_header";
-    public const string HomeBody = "home_body";
-    public const string Home = "home";
-    public const string StringValidation = "string_validation";
-    public const string CopyToClipboardSuccess = "copy_to_clipboard_success";
-    public const string NotStarted = "not_started";
-    public const string Uploading = "uploading";
-
-    // org
-    public const string OrgTooMany = "org_too_many";
-
-    // project
-    public const string ProjectInvalidHoursPerDay = "project_invalid_hours_per_day";
-    public const string ProjectInvalidDaysPerWeek = "project_invalid_days_per_week";
-    public const string ProjectFileLimitExceeded = "project_file_limit_exceeded";
-
-    // task
-    public const string TaskTooManyDescN = "task_too_many_descn";
-    public const string TaskCantMoveRootProjectNode = "task_cant_move_root_project_node";
-    public const string TaskRecursiveLoopDetected = "task_recursive_loop_detected";
-    public const string TaskMovePrevSibParentMismatch = "task_move_prev_sib_parent_mismatch";
-    public const string TaskDeleteProjectAttempt = "task_delete_project_attempt";
-    public const string TaskTooManyDescNToDelete = "task_too_many_descn_to_delete";
-
-    // vitem
-    public const string VItemInvalidTimeInc = "vitem_invalid_time_inc";
-    public const string VItemInvalidCostInc = "vitem_invalid_cost_inc";
-
-    // UI
-
-    // general
-    public const string Display = "display";
-    public const string User = "user";
-    public const string Time = "time";
-    public const string Cost = "cost";
-    public const string File = "file";
-    public const string SubCounts = "sub_counts";
-    public const string Minutes = "minutes";
-    public const string Hours = "hours";
-    public const string Days = "days";
-    public const string Weeks = "weeks";
-    public const string Years = "years";
-    public const string Unassigned = "unassigned";
-    public const string Task = "task";
-    public const string Parallel = "parallel";
-    public const string Sequential = "sequential";
-    public const string Comment = "comment";
-    public const string Description = "description";
-    public const string NoDescription = "no_description";
-    public const string NothingToSeeHere = "nothing_to_see_here";
-    public const string Loading = "loading";
-    public const string Min = "min";
-    public const string Max = "max";
-    public const string True = "true";
-    public const string False = "false";
-    public const string Required = "required";
-    public const string Public = "public";
-    public const string New = "new";
-    public const string Edit = "edit";
-    public const string Create = "create";
-    public const string Delete = "delete";
+{ 
+    public const string Active = "active";
     public const string Cancel = "cancel";
+    public const string ChildN = "child_n";
+    public const string Clear = "clear";
+    public const string Comment = "comment";
     public const string Confirm = "confirm";
-    public const string Name = "name";
-    public const string Currency = "currency";
-    public const string CreatedOn = "created_on";
-    public const string HoursPerDay = "hours_per_day";
-    public const string DaysPerWeek = "days_per_week";
-    public const string StartOn = "start_on";
-    public const string EndOn = "end_on";
-    public const string Note = "note";
-    public const string TimeMin = "time_min";
-    public const string TimeEst = "time_est";
-    public const string TimeInc = "time_inc";
+    public const string CopyToClipboardSuccess = "copy_to_clipboard_success";
+    public const string Cost = "cost";
     public const string CostEst = "cost_est";
     public const string CostInc = "cost_inc";
+    public const string Create = "create";
+    public const string CreatedOn = "created_on";
+    public const string Currency = "currency";
+    public const string Days = "days";
+    public const string DaysPerWeek = "days_per_week";
+    public const string Delete = "delete";
+    public const string DescN = "desc_n";
+    public const string Description = "description";
+    public const string Display = "display";
+    public const string Edit = "edit";
+    public const string EndOn = "end_on";
+    public const string False = "false";
+    public const string File = "file";
+    public const string FileLimit = "file_limit";
     public const string FileN = "file_n";
     public const string FileSize = "file_size";
-    public const string ChildN = "child_n";
-    public const string DescN = "desc_n";
-    public const string FileLimit = "file_limit";
-    public const string Upload = "upload";
+    public const string Home = "home";
+    public const string HomeBody = "home_body";
+    public const string HomeHeader = "home_header";
+    public const string Hours = "hours";
+    public const string HoursPerDay = "hours_per_day";
     public const string Invite = "invite";
-    public const string Active = "active";
-    public const string Clear = "clear";
-
-    // orgs
-    public const string OrgMyOrgs = "org_my_orgs";
-    public const string OrgNoOrgs = "org_no_orgs";
-    public const string OrgNewOrg = "org_new_org";
-    public const string OrgUpdateOrg = "org_update_org";
-    public const string OrgConfirmDeleteOrg = "org_confirm_delete_org";
-    public const string OrgName = "org_name";
-    public const string OrgYourName = "org_your_name";
-    public const string OrgNameProjects = "org_name_projects";
-    public const string OrgProjects = "org_projects";
-    public const string OrgNoProjects = "org_no_projects";
-    public const string OrgNewProject = "org_new_project";
-    public const string OrgUpdateProject = "org_update_project";
-    public const string OrgConfirmDeleteProject = "org_confirm_delete_project";
-    public const string OrgMembers = "org_members";
-    public const string OrgNoMembers = "org_no_members";
-    public const string OrgNewMember = "org_new_member";
-    public const string OrgUpdateMember = "org_update_member";
+    public const string Loading = "loading";
+    public const string Max = "max";
+    public const string Min = "min";
+    public const string Minutes = "minutes";
+    public const string Name = "name";
+    public const string New = "new";
+    public const string NoDescription = "no_description";
+    public const string NotStarted = "not_started";
+    public const string Note = "note";
+    public const string NothingToSeeHere = "nothing_to_see_here";
     public const string OrgConfirmDeactivateMember = "org_confirm_deactivate_member";
-
-    // org members
-    public const string OrgMemberInviteEmailSubject = "org_member_invite_email_subject";
+    public const string OrgConfirmDeleteOrg = "org_confirm_delete_org";
+    public const string OrgConfirmDeleteProject = "org_confirm_delete_project";
     public const string OrgMemberInviteEmailHtml = "org_member_invite_email_html";
+    public const string OrgMemberInviteEmailSubject = "org_member_invite_email_subject";
     public const string OrgMemberInviteEmailText = "org_member_invite_email_text";
     public const string OrgMemberRole = "org_member_role";
-    public const string OrgMemberRoleOwner = "org_member_role_owner";
     public const string OrgMemberRoleAdmin = "org_member_role_admin";
-    public const string OrgMemberRoleWriteAllProjects = "org_member_role_write_all_projects";
-    public const string OrgMemberRoleReadAllProjects = "org_member_role_read_all_projects";
+    public const string OrgMemberRoleOwner = "org_member_role_owner";
     public const string OrgMemberRolePerProject = "org_member_role_per_project";
-
-    // projects
-
-    // tasks
-    public const string TaskNewTask = "task_new_task";
-    public const string TaskUpdateTask = "task_edit_task";
-    public const string TaskConfirmDeleteTask = "task_confirm_delete_task";
-    public const string TaskMovingTask = "task_moving_task";
-    public const string TaskNewTime = "task_new_time";
-    public const string TaskNewCost = "task_new_cost";
-    public const string TaskUpdateTime = "task_update_time";
-    public const string TaskUpdateCost = "task_update_cost";
-    public const string TaskNoTimes = "task_no_times";
-    public const string TaskNoCosts = "task_no_costs";
-    public const string TaskConfirmDeleteVItem = "task_confirm_delete_vitem";
-    public const string TaskUploadFile = "task_upload_file";
-    public const string TaskNoFiles = "task_no_files";
+    public const string OrgMemberRoleReadAllProjects = "org_member_role_read_all_projects";
+    public const string OrgMemberRoleWriteAllProjects = "org_member_role_write_all_projects";
+    public const string OrgMembers = "org_members";
+    public const string OrgMyOrgs = "org_my_orgs";
+    public const string OrgName = "org_name";
+    public const string OrgNameProjects = "org_name_projects";
+    public const string OrgNewMember = "org_new_member";
+    public const string OrgNewOrg = "org_new_org";
+    public const string OrgNewProject = "org_new_project";
+    public const string OrgNoMembers = "org_no_members";
+    public const string OrgNoOrgs = "org_no_orgs";
+    public const string OrgNoProjects = "org_no_projects";
+    public const string OrgProjects = "org_projects";
+    public const string OrgTooMany = "org_too_many";
+    public const string OrgUpdateMember = "org_update_member";
+    public const string OrgUpdateOrg = "org_update_org";
+    public const string OrgUpdateProject = "org_update_project";
+    public const string OrgYourName = "org_your_name";
+    public const string Parallel = "parallel";
+    public const string ProjectFileLimitExceeded = "project_file_limit_exceeded";
+    public const string ProjectInvalidDaysPerWeek = "project_invalid_days_per_week";
+    public const string ProjectInvalidHoursPerDay = "project_invalid_hours_per_day";
+    public const string Public = "public";
+    public const string Required = "required";
+    public const string Sequential = "sequential";
+    public const string StartOn = "start_on";
+    public const string StringValidation = "string_validation";
+    public const string SubCounts = "sub_counts";
+    public const string Task = "task";
+    public const string TaskCantMoveRootProjectNode = "task_cant_move_root_project_node";
     public const string TaskConfirmDeleteFile = "task_confirm_delete_file";
+    public const string TaskConfirmDeleteTask = "task_confirm_delete_task";
+    public const string TaskConfirmDeleteVitem = "task_confirm_delete_vitem";
+    public const string TaskDeleteProjectAttempt = "task_delete_project_attempt";
+    public const string TaskEditTask = "task_edit_task";
+    public const string TaskMovePrevSibParentMismatch = "task_move_prev_sib_parent_mismatch";
+    public const string TaskMovingTask = "task_moving_task";
+    public const string TaskNewCost = "task_new_cost";
+    public const string TaskNewTask = "task_new_task";
+    public const string TaskNewTime = "task_new_time";
+    public const string TaskNoCosts = "task_no_costs";
+    public const string TaskNoFiles = "task_no_files";
+    public const string TaskNoTimes = "task_no_times";
+    public const string TaskRecursiveLoopDetected = "task_recursive_loop_detected";
+    public const string TaskTooManyDescn = "task_too_many_descn";
+    public const string TaskTooManyDescnToDelete = "task_too_many_descn_to_delete";
+    public const string TaskUpdateCost = "task_update_cost";
+    public const string TaskUpdateTime = "task_update_time";
+    public const string TaskUploadFile = "task_upload_file";
+    public const string Time = "time";
+    public const string TimeEst = "time_est";
+    public const string TimeInc = "time_inc";
+    public const string TimeMin = "time_min";
+    public const string True = "true";
+    public const string Unassigned = "unassigned";
+    public const string Upload = "upload";
+    public const string Uploading = "uploading";
+    public const string User = "user";
+    public const string VitemInvalidCostInc = "vitem_invalid_cost_inc";
+    public const string VitemInvalidTimeInc = "vitem_invalid_time_inc";
+    public const string Weeks = "weeks";
+    public const string Years = "years";
 }
